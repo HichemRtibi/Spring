b268deced5332ac8b866af7ec814e79592c9102b
hichem.rtibi <=hichem.rtibi@gmail.com>
2016-12-28 03:35:03 +0100

    configureation de projet avec oracle 11g

---
+++ Spring_Oracle11g/src/main/java/com/example/SpringOracle11gApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOracle11gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOracle11gApplication.class, args);
+	}
+}
